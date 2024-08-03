3d151ce76b69e6c2a1eed22deafa6d10c7109910
SergeG28071988 <sergeygalckin2012@yandex.ru>
2024-08-03 15:10:54 +0300

    Models has been created.

--- Shop/catalog/admin.py
+++ Shop/catalog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Component
+from django.utils.html import format_html, mark_safe
 
 # Register your models here.
+
+
+@admin.register(Component)
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'show_photo')
+    fields = ['name', 'description', 'category', ('price', 'photo', 'date_created', 'show_photo')]
+    readonly_fields = ["show_photo"]
+
+    def show_photo(self, obj):
+        if obj.photo:
+            return format_html(
+                f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+            # можно и с использованием функции mark_safe
+            # return mark_safe(
+            # f'<img src="{obj.photo.url}" style="max-height: 100px;">')
+        else:
+            return "Фото не доступно"
+
+    show_photo.short_description = 'Фото товара'
--- Shop/catalog/models.py
+++ Shop/catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Component(models.Model):
+    objects = None
+    name = models.CharField(max_length=100, help_text="Введите товар", verbose_name="Товар")
+    description = models.TextField(help_text="Введите описание товара", verbose_name="Описание товара")
+    CATEGORY_CHOICES = [
+        ('Процессоры', 'Процессоры'),
+        ('Материнские платы', 'Материнские платы'),
+        ('Видеокарты', 'Видеокарты'),
+        ('Оперативная память', 'Оперативная память'),
+        ('SSD-диски', 'SSD-диски'),
+        ('HDD-диски', 'HDD-диски'),
+        ('Блоки питания>', 'Блоки питания'),
+    ]
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, verbose_name='Категория')
+
+    price = models.DecimalField(help_text="Введите цену товара", verbose_name="Цена товара", max_digits=10,
+                                decimal_places=2)
+    photo = models.ImageField(upload_to='images',
+                              help_text="Введите фото",
+                              verbose_name="Фото товара",
+                              null=True, blank=True)
+    date_created = models.DateField(help_text='Введите дату добавления', verbose_name='Дата добавления')
+
+    def __str__(self):
+        return f'Наименование: {self.name} Описание: {self.description}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
